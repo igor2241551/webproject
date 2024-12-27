f8e9cd4a742667f232ac739ce56cf3db1311c01f
diogojacinto <2241541@my.ipleiria.pt>
2024-12-27 18:23:36 +0000

    Navbar e footer tanto da sobre.html como da detalhespais.html feitos. Funcionalidades e design da detalhespais.html completo.

--- assets/js/scriptdetalhespais.js
+++ assets/js/scriptdetalhespais.js
@@ -11,21 +11,23 @@ document.addEventListener("DOMContentLoaded", function () {
         method: "GET",
         url: apiUrl,
         success: function (data) {
-            // Valida a resposta da API
-            if (!data || data.length === 0) {
-                alert("Os dados do país não foram encontrados.");
-                return;
-            }
             const pais = data[0]; // A API retorna um array com os dados do país pedido (array com objetos dentro)
-
-            // Preenche os campos do formulário com validações (se a API não retornar qualquer campo, retorna "N/A")                                
-            document.getElementById("nome").value = pais.name?.common || "N/A";
+    
+            //Campo do formulário
+            document.getElementById("nome").innerText = pais.name?.common || "N/A";
             document.getElementById("capital").value = pais.capital?.[0] || "N/A";
-            document.getElementById("moeda").value = pais.currencies ?   Object.values(pais.currencies).map((currency) => currency.name).join(", "): "N/A";
+            document.getElementById("moeda").value = pais.currencies ? Object.values(pais.currencies).map((currency) => currency.name).join(", ") : "N/A";
             document.getElementById("continente").value = pais.region || "N/A";
-            document.getElementById("populacao").value = pais.population ? pais.population.toLocaleString(): "N/A";
-            document.getElementById("linguas").value = pais.languages ? Object.values(pais.languages).join(", "): "N/A";
+            document.getElementById("populacao").value = pais.population ? pais.population.toLocaleString() : "N/A";
+            document.getElementById("linguas").value = pais.languages ? Object.values(pais.languages).join(", ") : "N/A";
             document.getElementById("bandeira").src = pais.flags?.svg || "";
+    
+            // CAMPO DAS INFORMAÇÕES ADICIONAIS
+            document.getElementById("area").innerHTML = `<strong>Área:</strong> ${pais.area ? pais.area.toLocaleString() + " km²" : "N/A"}`;
+            document.getElementById("subregiao").innerHTML = `<strong>Sub-região:</strong> ${pais.subregion || "N/A"}`;
+            const membroONU = pais.unMember ? "Sim" : "Não";
+            document.getElementById("uniaoeuropeia").innerHTML = `<strong>Membro da ONU:</strong> ${membroONU}`;
+            document.getElementById("populacao-urbana").innerHTML = `<strong>População:</strong> ${pais.population ? pais.population.toLocaleString() : "N/A"}`
         },
         error: function (error) {
             console.error("Erro ao pedir dados à API:", error);
@@ -33,4 +35,5 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 });
+
 
